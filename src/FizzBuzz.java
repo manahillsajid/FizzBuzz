c857206d088da6afb2cc0c738b5ba9b90562bc50
manahillsajid <manahill.sajid@mail.utoronto.ca>
2024-09-09 17:36:44 -0400

    Refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,10 +1,31 @@
 /**
  * Solve the FizzBuzz challenge.
  */
+
+/**
+ * java's and syntax = &&
+ * similar:
+ * - if statement structure
+ * - same variable types (boolean and int)
+ * different:
+ * - have to write variable type before variable declaration
+ * - need to have a main method where all the code is run
+ * - System.out.println() (java) vs. print() (python)
+ */
 class FizzBuzz {
 
     public static void main(String[] args) {
 
+ //     doFizzBuzzfor();
+        int i = 1;
+        while (i <= 100) {
+            i = doFizzBuzz(i);
+        }
+
+
+    }
+
+    private static void doFizzBuzzfor() {
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
@@ -30,5 +51,21 @@ class FizzBuzz {
 
             }
         }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+        i++;
+        return i;
     }
 }
